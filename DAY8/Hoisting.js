15dc3478f2e0bdbbe3d2c246b6fe3e1b97f8b7e6
akash666374 <akashkushal6976@gmail.com>
2026-09-07 19:38:46 +0530

    DAY8 event loops

---
+++ DAY8/Hoisting.js
@@ -0,0 +1,49 @@
+console.log("HELLO");
+console.log(a);
+var a = 10;
+console.log(a);
+console.log(b);
+var b = "HELLO"
+console.log(b);
+
+
+let x = 20;
+console.log(x);
+
+
+console.log(z);
+const z = 30;
+
+
+
+//using functions
+
+// greet();
+
+// function greet() {
+//     console.log("HELLO");
+// }
+
+// greet();
+
+// var greet = function () {
+//     console.log("HELLO");
+// };
+
+
+// greet();
+
+// let greet = function () {
+//     console.log("HELLO");
+// };
+
+
+
+
+
+
+
+
+
+
+
