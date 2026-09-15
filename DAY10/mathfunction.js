4e9137aecc06e960bc835b90a309732bf3ffc41f
akash666374 <akashkushal6976@gmail.com>
2026-09-15 08:02:56 +0530

    DAY10 TASK

---
+++ DAY10/mathfunction.js
@@ -0,0 +1,32 @@
+// let num = -2.5;
+// console.log(Math.round(num));  // it takes us to the nearest value
+
+// let num = 8.99;
+// console.log(Math.floor(num));  // it removes the decimal
+
+// let num = 5.6;
+// console.log(Math.ceil(num));  // it round of the value
+
+// console.log(Math.random()); // it creates a random decimal number from 0 to 1
+
+// let a = 50;
+// let b = 80;
+// console.log(Math.max(a, b)); // it returns the largest value
+// console.log(Math.min(a, b));  // it returns the smallestt value
+
+
+// let balance = -500;
+// console.log(Math.abs(balance));  //it removes the neagative value and returns the same value
+
+// let a = 112
+// console.log(Math.sqrt(a)); // it returns the square root of that number
+
+
+// console.log(Math.pow(5, 2));  it multiply the given value
+// console.log(Math.max());
+
+// console.log(Math.min());
+
+
+
+
